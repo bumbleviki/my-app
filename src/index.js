04acc971afa53582a56e47a8b5727aac0afc183a
bumbleviki <puka.viktorija@gmail.com>
2022-09-05 20:55:14 +0300

    Final changes

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiKey = "7bbed1df0cbec2753536f3df96f6ac40";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
